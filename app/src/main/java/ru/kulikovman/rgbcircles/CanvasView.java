958eaec2315fc3109904fdaf490ce18f6e92d27c
Maksim Kulikov <kulikovman@gmail.com>
2017-03-18 10:27:55 +0500

    Centering of the main circle

--- app/src/main/java/ru/kulikovman/rgbcircles/CanvasView.java
+++ app/src/main/java/ru/kulikovman/rgbcircles/CanvasView.java
@@ -2,17 +2,31 @@ package ru.kulikovman.rgbcircles;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class CanvasView extends View {
+    private static int width;
+    private static int height;
     private GameManager gameManager;
 
     public CanvasView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        gameManager = new GameManager();
+        initWidthAndHeight(context);
+        gameManager = new GameManager(this, width, height);
+    }
+
+    private void initWidthAndHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        width = point.x;
+        height = point.y;
     }
 
     @Override
